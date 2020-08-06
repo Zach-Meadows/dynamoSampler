592953ef944f71a17ca837df1d79b02c86c428ff
Zach Meadows <zmeadows1@gmail.com>
2020-08-06 10:12:00 -0400

    dynamo sampler

---
+++ putItem.js
@@ -0,0 +1,24 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'us-east-1'});
+
+// Create the DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+var params = {
+  TableName: 'Videos',
+  Item: {
+    'videoID' : {S: 'Ajfalkub084'},
+    'uploadDate' : {S: '10/12/20'}
+  }
+};
+
+// Call DynamoDB to add the item to the table
+ddb.putItem(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
+});
